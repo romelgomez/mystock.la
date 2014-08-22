8f0e9f6754c8db647380c58f6e1ef0addbc48418
romelgomez <bmxquiksilver7185@gmail.com>
2014-08-21 23:20:25 -0430

    routine task

--- app/Controller/CategoriesController.php
+++ app/Controller/CategoriesController.php
@@ -1,12 +1,13 @@
 <?php class CategoriesController extends AppController{
 
-    /* Descripción: 		Función recursiva interna, Para personalisar el arbol.
+    /* Descripción: 		Función recursiva interna, Para personalizar el árbol.
      * tipo de solicitud: 	interna de la clase CategoriesController
      * tipo de acceso: 		administrativo
-     * Recive: 				un array anidado de categotias.
-     * Retorna: 			un JSON  anidado de categorias, compatible con la libreria tree.jquery.js
+     * Recibe: 				un array anidado de categorías.
+     * Retorna: 			un JSON  anidado de categorías, compatible con la librería tree.jquery.js
      *******************/
     private function recursive_tree($tree){
+        $data = array();
         foreach($tree as $key => $val){
 
             // para efecto de corregir errores
@@ -28,16 +29,16 @@
         return $data;
     }
 
-    /* Descripción: Función permite obtener el arbol listo para enviar a la vista.
+    /* Descripción: Función permite obtener el árbol listo para enviar a la vista.
      * tipo de solicitud: 	internar
      * tipo de acceso: 		administrativo
-     * Recive: 				null
+     * Recibe: 				null
      * Retorna: 			un array
      *******************/
     private function categories(){
-        $return['length'] = $this->Category->find('count');
+        $return['length'] = $this->{'Category'}->find('count');
         if($return['length']){
-            $categories = $this->Category->find('threaded', array(
+            $categories = $this->{'Category'}->find('threaded', array(
                 'order' => 'Category.lft',
                 'contain' => false
             ));
@@ -55,32 +56,31 @@
      *******************/
     public function index(){
 
-        $return['total'] = $this->Category->find('count');
-
+        $return['total'] = $this->{'Category'}->find('count');
 
         if($return['total'] > 0){
-            $categories = $this->Category->find('threaded', array(
+            $categories = $this->{'Category'}->find('threaded', array(
                 'order' => 'Category.lft',
                 'contain' => false
             ));
             $return['categories'] = json_encode($this->recursive_tree($categories));
         }
 
-        $this->set('categories',$return);
+        $this->{'set'}('categories',$return);
 
     }
 
-    /* Descripción: Función para crear nuevas categorias.
+    /* Descripción: Función para crear nuevas categorías.
      * tipo: 				ajax-post,ajax-get
      * tipo de acceso: 		administrativo
-     * Recive:			 	array
+     * Recibe:			 	array
      * Retorna: 			un array, el cual sera transformado en un objeto JSON en la vista ajax_view.
      *******************/
     public function new_category(){
-        if($this->request->is('post')){
-            $request = $this->request->data;
+        if($this->{'request'}->is('post')){
+            $request = $this->{'request'}->data;
         }else{
-            $request = $this->request->query;
+            $request = $this->{'request'}->query;
         }
 
         $category =	array(
@@ -91,9 +91,9 @@
             )
         );
 
-        $this->Category->set($category);
-        if($this->Category->validates()){
-            if($this->Category->save($category)){
+        $this->{'Category'}->set($category);
+        if($this->{'Category'}->validates()){
+            if($this->{'Category'}->save($category)){
                 $return['save'] = true;
             }else{
                 $return['save'] = false; // ++++++++++++++ ha ocurrido un error +++++++++++++++
@@ -102,8 +102,8 @@
 
         }else{
             $return['validates'] = false;
-            // se envia los campos faltantes o algun error que pudicece haber ocurrido.
-            $errors = $this->Category->validationErrors;
+            // se envía los campos faltantes o algún error que pudiese haber ocurrido.
+            $errors = $this->{'Category'}->validationErrors;
             foreach($errors as $k=>$v){
                 $tmp = 'category_'.$k;
                 $return['fields'][Inflector::camelize($tmp)] = $v[0];
@@ -112,21 +112,21 @@
 
         $return += $this->categories();
 
-        $this->set('return',$return);
-        $this->render('ajax_view','ajax');
+        $this->{'set'}('return',$return);
+        $this->{'render'}('ajax_view','ajax');
     }
 
-    /* Descripción: Función para editar el nombre de la categoria.
+    /* Descripción: Función para editar el nombre de la categoría.
      * tipo: 				ajax-post, ajax-get
      * tipo de acceso: 		administrativo
-     * Recive:			 	array
+     * Recibe:			 	array
      * Retorna: 			un array, el cual sera transformado en un objeto JSON en la vista ajax_view.
      *******************/
     public function edit_category_name(){
-        if($this->request->is('post')){
-            $request = $this->request->data;
+        if($this->{'request'}->is('post')){
+            $request = $this->{'request'}->data;
         }else{
-            $request = $this->request->query;
+            $request = $this->{'request'}->query;
         }
 
         $category = array(
@@ -136,10 +136,10 @@
             )
         );
 
-        $this->Category->set($category);
-        if($this->Category->validates()){
-            if($this->Category->save($category)){
-                $return = $this->Category->read();
+        $this->{'Category'}->set($category);
+        if($this->{'Category'}->validates()){
+            if($this->{'Category'}->save($category)){
+                $return = $this->{'Category'}->read();
                 $return['save'] = true;
             }else{
                 $return['save'] = false; // ++++++++++++++ ha ocurrido un error +++++++++++++++
@@ -147,8 +147,8 @@
             $return['validates'] = true;
         }else{
             $return['validates'] = false;
-            // se envia los campos faltantes o algun error que pudicece haber ocurrido.
-            $errors = $this->Category->validationErrors;
+            // se envía los campos faltantes o algún error que pudiese haber ocurrido.
+            $errors = $this->{'Category'}->validationErrors;
             foreach($errors as $k=>$v){
                 $tmp = 'category_'.$k;
                 $return['fields'][Inflector::camelize($tmp)] = $v[0];
@@ -157,150 +157,150 @@
 
         $return += $this->categories();
 
-        $this->set('return',$return);
-        $this->render('ajax_view','ajax');
+        $this->{'set'}('return',$return);
+        $this->{'render'}('ajax_view','ajax');
     }
 
-    /* Descripción: Función para borra una categoria y sus categorias hijas si es requerido. Actualiza todo el arbol cada vez que es llamada.
+    /* Descripción: Función para borra una categoría y sus categorías hijas si es requerido. Actualiza todo el árbol cada vez que es llamada.
      * tipo: 				ajax-post, ajax-get
      * tipo de acceso: 		administrativo
-     * Recive:			 	array
+     * Recibe:			 	array
      * Retorna: 			un array, el cual sera transformado en un objeto JSON en la vista ajax_view.
     *******************/
     public function delect_category(){
-        if($this->request->is('post')){
-            $request = $this->request->data;
+        if($this->{'request'}->is('post')){
+            $request = $this->{'request'}->data;
         }else{
-            $request = $this->request->query;
+            $request = $this->{'request'}->query;
         }
 
         $theWholeBranch 	= $request['theWholeBranch'];
         $id					= $request['id'];
 
-        $category = $this->Category->find('first', array(
+        $category = $this->{'Category'}->find('first', array(
             'conditions' => array('Category.id' => $id),
             'contain' => false
         ));
 
         if($category){
             if($theWholeBranch == 1){
-                $this->Category->delete();
+                $this->{'Category'}->delete();
             }else{
-                $this->Category->removeFromTree($id, true);
+                $this->{'Category'}->removeFromTree($id, true);
             }
         }
 
         $return = $this->categories();
 
-        $this->set('return',$return);
-        $this->render('ajax_view','ajax');
+        $this->{'set'}('return',$return);
+        $this->{'render'}('ajax_view','ajax');
     }
 
-    /* Descripción: Función para cambiar la posición de la categoria en el arbol, pricipalmente tiene dos secciones 1) solo mover de arriba a abajo y visiversa 2) primero establecer el parent_id y luego mover de arriba a abajo y visiversa
-     * tipo: 				ajax-post, ajax-get
-     * tipo de acceso: 		administrativo
-     * Recive:			 	array
-     * Retorna: 			un array, el cual sera transformado en un objeto JSON en la vista ajax_view.
+    /* Descripción: Función para cambiar la posición de la categoría en el árbol, principalmente tiene dos secciones 1) solo mover de arriba a abajo y viceversa 2) primero establecer el parent_id y luego mover de arriba a abajo y viceversa
+     * tipo:                ajax-post, ajax-get
+     * tipo de acceso:      administrativo
+     * Recibe:              array
+     * Retorna:             un array, el cual sera transformado en un objeto JSON en la vista ajax_view.
     *******************/
     public function edit_category_position(){
-        if($this->request->is('post')){
-            $request = $this->request->data;
+        if($this->{'request'}->is('post')){
+            $request = $this->{'request'}->data;
         }else{
-            $request = $this->request->query;
+            $request = $this->{'request'}->query;
         }
 
         //print_r($request);
 
         if($request['type'] == 'only_move'){
             $id 			= (int)$request['id'];
-            $category = $this->Category->find('first', array(
+            $category = $this->{'Category'}->find('first', array(
                 'conditions' => array('Category.id' => $id),
                 'contain' => false
             ));
             if($category){
-                $positions = $this->Category->find('count', array(
+                $positions = $this->{'Category'}->find('count', array(
                     'conditions' => array('Category.lft >=' => $request['min'],'Category.rght <=' => $request['max'],'Category.parent_id' => $request['parent_id'])
                 ));
                 if($positions > 0){
                     if($request['move_to'] == 'moveDown'){
-                        $return['result'] = $this->Category->moveDown($id, $positions);
+                        $return['result'] = $this->{'Category'}->moveDown($id, $positions);
                     }
                     if($request['move_to'] == 'moveUp'){
-                        $return['result'] = $this->Category->moveUp($id, $positions);
+                        $return['result'] = $this->{'Category'}->moveUp($id, $positions);
                     }
                 }
             }
         }
         if($request['type'] == 'set_parent_and_move'){
 
-            $new_parent_id 			= 	(int)$request['new_parent_id'];
-            $moved_node_id			=	(int)$request['moved_node_id'];
-            $target_node_id			=	(int)$request['target_node_id'];
-            $position				=	$request['position'];
+            $new_parent_id          =   (int)$request['new_parent_id'];
+            $moved_node_id          =   (int)$request['moved_node_id'];
+            $target_node_id         =   (int)$request['target_node_id'];
+            $position               =   $request['position'];
 
             $category = array(
                 'Category'=>array(
-                    'id'		=>$moved_node_id,
-                    'parent_id'	=>$new_parent_id
+                    'id'        =>$moved_node_id,
+                    'parent_id' =>$new_parent_id
                 )
             );
 
             if($position == 'inside'){
                 /*  Descripción:
-                 *  antes de insertar la categoria observamos cuantos hijos tiene la categoria que sera populada, es decir cuantos hijos tiene target_node_id,
-                 *  si no tiene ninguno la categoria simplemente se insert,  si tiene hijos el valor que arroje sera el numero de posiciones que la categoria tendra que subir para estar de primera.
-                 *  es importante recordar que la categoria al ser insetada o a establecerle un nuevo parent_id es ordenada de ultima.
+                 *  antes de insertar la categoría observamos cuantos hijos tiene la categoría que sera populada, es decir cuantos hijos tiene target_node_id,
+                 *  si no tiene ninguno la categoría simplemente se insert,  si tiene hijos el valor que arroje sera el numero de posiciones que la categoría tendrá que subir para estar de primera.
+                 *  es importante recordar que la categoría al ser insertada o a establecerle un nuevo parent_id es ordenada de ultima.
                 */
 
-                $position_length = $this->Category->childCount($target_node_id, true);
+                $position_length = $this->{'Category'}->childCount($target_node_id, true);
 
-                if($this->Category->save($category)){
+                if($this->{'Category'}->save($category)){
                     $return['save_new_parent_id'] 	= true;
                 }
 
                 if($position_length == 0){
                     $return['status'] 				=  'se mantiene';
                 }elseif($position_length > 0){
-                    $this->Category->moveUp($moved_node_id, $position_length);
-                    $return['status'] 				=  'subio '.$position_length.' posiciones.';
+                    $this->{'Category'}->moveUp($moved_node_id, $position_length);
+                    $return['status'] 				=  'subió '.$position_length.' posiciones.';
                 }
             }
 
             if($position == 'before'){
                 /* Descripción:
-                 * Antes de estableser el parent_id se cuenta cuantas categorias existen con parent_id == null tal valor
-                 * reprecenta el numero de posiciones que la categoria sera subida para posicionarce de primera.
-                 * es importante recordar que posicion es before solo cuando la categoria es posicionada de primera sin padres es decir es un caso unico.
+                 * Antes de establecer el parent_id se cuenta cuantas categorías existen con parent_id == null tal valor
+                 * representa el numero de posiciones que la categoría sera subida para posicionarse de primera.
+                 * es importante recordar que posición es before solo cuando la categoría es posicionada de primera sin padres es decir es un caso unico.
                 */
 
-                $position_length = $this->Category->find('count',array(
+                $position_length = $this->{'Category'}->find('count',array(
                     'conditions' => array('Category.parent_id' => null)
                 ));
 
-                if($this->Category->save($category)){
+                if($this->{'Category'}->save($category)){
                     $return['save_new_parent_id'] 	= true;
                 }
 
-                $this->Category->moveUp($moved_node_id, $position_length);
-                $return['status'] 	=  'subio '.$position_length.' posiciones.';
+                $this->{'Category'}->moveUp($moved_node_id, $position_length);
+                $return['status'] 	=  'subió '.$position_length.' posiciones.';
             }
 
             if($position == 'after'){
                 /* Descripción:
-                 * La categoria tiene dos opciones mantenerce o subir, si la categoria es sucesiva se ha de suponer que el admin la coloco de ultima por lo tanto no es necesario subir la categoria
-                 * se calcula el minimo y maximo junto con el parent_id de la categoria movida permitira consulta cuantas categoria directas (directChildren) existen entre la categoria movida y la sucesiva o target.
+                 * La categoría tiene dos opciones mantenerse o subir, si la categoría es sucesiva se ha de suponer que el admin la coloco de ultima por lo tanto no es necesario subir la categoría
+                 * se calcula el mínimo y maximo junto con el parent_id de la categoría movida permitirá consulta cuantas categoría directas (directChildren) existen entre la categoría movida y la sucesiva o target.
                  */
 
-                if($this->Category->save($category)){
+                if($this->{'Category'}->save($category)){
                     $return['save_new_parent_id'] 	= true;
                 }
 
-                $moved_node = $this->Category->find('first', array(
+                $moved_node = $this->{'Category'}->find('first', array(
                     'conditions' => array('Category.id' => (int)$moved_node_id),
                     'contain' => false
                 ));
 
-                $target_node = $this->Category->find('first', array(
+                $target_node = $this->{'Category'}->find('first', array(
                     'conditions' => array('Category.id' => (int)$target_node_id),
                     'contain' => false
                 ));
@@ -309,13 +309,13 @@
                 $min 		= $target_node['Category']['rght']+1;
                 $parent_id	= $moved_node['Category']['parent_id'];
 
-                $position_length = $this->Category->find('count', array(
+                $position_length = $this->{'Category'}->find('count', array(
                     'conditions' => array('Category.lft >=' => $min,'Category.rght <=' => $max,'Category.parent_id' => $parent_id)
                 ));
 
                 if($position_length > 0){
-                    $this->Category->moveUp($moved_node_id, $position_length);
-                    $return['status'] = 'subio '.$position_length.' posiciones.';
+                    $this->{'Category'}->moveUp($moved_node_id, $position_length);
+                    $return['status'] = 'subió '.$position_length.' posiciones.';
                 }else{
                     // son sucesivos
                     $return['status'] =  'se mantiene';
@@ -331,8 +331,8 @@
             $return = null;
         }
 
-        $this->set('return',$return);
-        $this->render('ajax_view','ajax');
+        $this->{'set'}('return',$return);
+        $this->{'render'}('ajax_view','ajax');
     }
 
 
@@ -376,8 +376,8 @@
 
         }
 
-        $return['path'] 				= $_path;
-        $return['id'] 					= $category_id;
+        $return['path']                 = $_path;
+        $return['id']                   = $category_id;
 
         $this->{'set'}('return',$return);
         $this->{'render'}('ajax_view','ajax');
