63fb16ca8fecb2311dea89ba00562a6273f54653
romelgomez <bmxquiksilver7185@gmail.com>
2014-11-22 00:04:32 -0430

    improve of user logic in general

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -2,17 +2,45 @@
 App::uses('CakeEmail', 'Network/Email');
 class UsersController extends AppController{
 
-    public function beforeFilter(){
-        $this->{'Auth'}->allow(array('add','check_email','in','recover_account','terms_of_service','privacy_policy','verify_email_address','send_email_again'));
+	/*
+	 @Name              -> beforeFilter
+	 @Description       -> This is CakePHP function. Check docs for more details.
+	 */
+	public function beforeFilter(){
+        $this->{'Auth'}->allow(array('add','check_email','in','recover_account','terms_of_service','privacy_policy','new_password_request','verify_email_address','send_email_again_to_verify_email_address'));
         parent::beforeFilter();
     }
 
+	/*
+	 @Name              -> privacy_policy
+	 @Description       -> The privacy policy.
+	 @RequestType	    -> GET
+	 @Parameters        -> NULL
+	 @Receives       	-> NULL
+	 @Returns           -> NULL
+	 */
 	public  function terms_of_service(){
 	}
 
+	/*
+	 @Name              -> privacy_policy
+	 @Description       -> The privacy policy.
+	 @RequestType	    -> GET
+	 @Parameters        -> NULL
+	 @Receives       	-> NULL
+	 @Returns           -> NULL
+	 */
 	public  function privacy_policy(){
 	}
 
+	/*
+	 @Name              -> login
+	 @Description       -> The method to represent Sign in forms. The (in) Ajax method is login real action.
+	 @RequestType	    -> GET
+	 @Parameters        -> NULL
+	 @Receives       	-> NULL
+	 @Returns           -> NULL
+	 */
     public function login(){
 		if($this->{'Auth'}->login()){
 			$this->{'redirect'}('/');
@@ -20,15 +48,30 @@ class UsersController extends AppController{
 		$this->response->disableCache();
     }
 
+	/*
+	 @Name              -> logout
+	 @Description       -> sign out.
+	 @RequestType	    -> GET
+	 @Parameters        -> NULL
+	 @Receives       	-> NULL
+	 @Returns           -> NULL
+	 */
     public function logout(){
         $this->{'Auth'}->logout();
         $this->{'redirect'}('/');
     }
 
-    public function in(){
+	/*
+	 @Name              -> in
+	 @Description       -> Login real action. Sign in method.
+	 @RequestType	    -> AJAX-POST
+	 @Parameters        -> NULL
+	 @Receives       	-> JSON object
+	 @Returns           -> Array, which is presented as json string with the json_encode() function in "ajax_view" view, in the ajax layout.
+	 */
+	public function in(){
         $request = $this->{'request'}->input('json_decode',true);
 
-		// Before login, first we need verify your email address, we already send to you one email to do that.
 		$options = array(
             'conditions' => array(
                 'User.email' 		=> $request['email'],
@@ -37,102 +80,170 @@ class UsersController extends AppController{
 
 		$user = $this->{'User'}->find('first',$options);
 
-		$inHash = Security::hash($request['password'], 'blowfish', $user['User']['password']);
+		$passwordHash = Security::hash($request['password'], 'blowfish', $user['User']['password']);
 
-        if($inHash === $user['User']['password']){
-
+		// checks that the user password match
+		if($passwordHash === $user['User']['password']){
+			// checks that the user is not banned
 			if(!$user['User']['banned']){
+				// checks that the user has email already verified
 				if($user['User']['email_verified']){
 					if($this->{'Auth'}->login($user)){
-						$return['login'] = true;
+						$return['status'] = 'success';
 					}else{
-						$return['login'] = false;
+						$return['status'] = 'error';
+						$return['message'] = 'no-login';
 					}
 				}else{
-					$return['email_verified'] 	= false;
+					$return['status'] = 'error';
+					$return['message'] = 'email-not-verified';
 				}
 			}else{
-				$return['banned'] 	= true;
+				$return['status'] = 'error';
+				$return['message'] = 'banned';
 			}
         }else{
-            $return['login'] = false;
-        }
+			$return['status'] = 'error';
+			$return['message'] = 'password-does-not-match';
+		}
 
 		$this->{'set'}('return',$return);
         $this->{'render'}('ajax_view','ajax');
     }
 
 	public function verify_email_address(){
-		debug($this->{'request'});
+		$request = $this->{'request'}->params;
 
-		//			$newHash = Security::hash($newPassword, 'blowfish', $storedPassword);
+		$options = array(
+				'conditions' => array(
+						'User.id' 		=> $request['id'],
+				)
+		);
 
-	}
+		$user = $this->{'User'}->find('first',$options);
 
-	public function send_email_again(){
-		debug($this->{'request'});
+		// checks that the user exist
+		if($user){
+			// checks that the email is not verified
+			if(!$user['User']['email_verified']){
 
-//		$options = array(
-//				'conditions' => array(
-//						'User.email' 		=> $this->{'request'}['data']['email'],
-//				)
-//		);
-//
-//		$user = $this->{'User'}->find('first',$options);
-//
-//		if($user){
-//
-//			$publicKey	 	= String::uuid();
-//			$privateKeyHash = Security::hash($publicKey);
-//
-//			$user =	array(
-//					'User'=>Array
-//					(
-//							'temp_password'			=>	$privateKeyHash,
-//					)
-//			);
-//
-//			if($this->{'User'}->save($user)){
-//				$return = $this->{'User'}->read();
-//
-//				$message = '<h3>MyStock.LA - Verify your email address</h3>'.
-//						'<p>Click in the next link to verify your email address:</p>'.
-//						'<p><a href="http://www.mystock.la/ve/'.(string)$return['User']['id'].'/'.(string)$publicKey.'" target="_blank">Verify my email address!</a></p>'.
-//						'<p>NOTE: After successfully verify your email address, please delete this email.<p>';
-//
-//				$Email = new CakeEmail();
-//				$Email->from(array('support@mystock.la' => 'MyStock.LA'));
-//				$Email->to($user['User']['email']);
-//				$Email->subject('MyStock.LA - Verify your email address');
-//
-//				if ($Email->send($message)) {
-//					$return['send'] = true;
-//				} else {
-//					$return['send'] = false;
-//				}
-//
-//			}
-//		}
+				$tempPasswordHash = Security::hash($request['key'], 'blowfish', $user['user']['temp_password']);
 
-	}
+				if($tempPasswordHash===$user['user']['temp_password']){
+					$user =	array(
+						'User'=>Array
+						(
+							'email_verified' =>	1,
+						)
+					);
 
-	public function send_email($config,$from,$to,$subject,$message){
-		$Email = new CakeEmail($config);
-		$Email->from($from);
-		$Email->to($to);
-		$Email->subject($subject);
+					if($this->{'User'}->save($user)){
+						$return['status'] = 'success';
+					}else{
+						$return['status'] = 'error';
+						$return['message'] = 'cannot-set-new-parameter';
+					}
 
-		return $Email->send($message);
+				}else{
+					$return['status'] = 'error';
+					$return['message'] = 'this-link-is-invalid';
+				}
+
+			}else{
+				$return['status'] = 'error';
+				$return['message'] = 'already-verified';
+			}
+		}else{
+			$return['status'] = 'error';
+			$return['message'] = 'user-not-exist';
+		}
+
+		$this->{'set'}('data',$return);
 	}
 
 
+	/*
+	 @Name              -> send_email_again_to_verify_email_address
+	 @Description       -> send email again to verify email address
+	 @RequestType	    -> AJAX-POST
+	 @Parameters        -> NULL
+	 @Receives       	-> JSON object
+	 @Returns           -> Array, which is presented as json string with the json_encode() function in "ajax_view" view, in the ajax layout.
+	 */
+	public function send_email_again_to_verify_email_address(){
+		$request = $this->{'request'}->input('json_decode',true);
+		$return = array();
+
+		$options = array(
+				'conditions' => array(
+						'User.email' 		=> $request['email'],
+				)
+		);
+
+		$user = $this->{'User'}->find('first',$options);
+
+		// checks that the user exist
+		if($user){
+
+			// checks that the email is not verified
+			if(!$user['User']['email_verified']){
+				$publicKey	 	= String::uuid();
+				$privateKeyHash = Security::hash($publicKey);
+
+				$user =	array(
+						'User'=>Array
+						(
+								'temp_password'			=>	$privateKeyHash,
+						)
+				);
+
+				if($this->{'User'}->save($user)){
+					$userData = $this->{'User'}->read();
+
+					$Email = new CakeEmail('default');
+					$Email->template('verifyEmail', 'verifyEmail');
+					$Email->viewVars(array('userId' => $userData['User']['id'],'publicKey'=>$publicKey));
+					$Email->emailFormat('both');
+					$Email->from(array('support@mystock.la' => 'MyStock.LA'));
+					$Email->to($userData['User']['email']);
+					$Email->subject('MyStock.LA - Verify your email address');
+
+					if ($Email->send()) {
+						$return['status'] = 'success';
+					}else{
+						$return['status'] = 'error';
+						$return['message'] = 'email-not-send';
+					}
+				}else{
+					$return['status'] = 'error';
+					$return['message'] = 'cannot-set-new-parameters';
+				}
+			}else{
+				$return['status'] = 'error';
+				$return['message'] = 'already-verified';
+			}
+		}else{
+			$return['status'] = 'error';
+			$return['message'] = 'user-not-exist';
+		}
+
+	}
+
+	/*
+	 @Name              -> add
+	 @Description       -> add new user
+	 @RequestType	    -> AJAX-POST
+	 @Parameters        -> NULL
+	 @Receives       	-> JSON object
+	 @Returns           -> Array, which is presented as json string with the json_encode() function in "ajax_view" view, in the ajax layout.
+	 */
     public function add(){
         $request = $this->{'request'}->input('json_decode',true);
+		$return = array();
 
 		$this->{'User'}->set($request);
         if($this->{'User'}->validates()){
 
-			// Before login, first we need verify your email address, we already send to you one email to do that.
 			$options = array(
 					'conditions' => array(
 							'User.email' 		=> $request['email'],
@@ -141,6 +252,7 @@ class UsersController extends AppController{
 
 			$user = $this->{'User'}->find('first',$options);
 
+			// checks that the user does not exist
 			if(!$user){
 				Security::setHash('blowfish');
 				$passwordHash = Security::hash($request['password']);
@@ -161,51 +273,56 @@ class UsersController extends AppController{
 				);
 
 				if($this->{'User'}->save($user)){
-					$return = $this->{'User'}->read();
+					$userData = $this->{'User'}->read();
 
-					// Send Email
-					$message = '<h3>MyStock.LA - Verify your email address</h3>'.
-							'<p>Click in the next link to verify your email address:</p>'.
-							'<p><a href="http://www.mystock.la/ve/'.(string)$return['User']['id'].'/'.(string)$publicKey.'" target="_blank">Verify my email address!</a></p>'.
-							'<p>NOTE: After successfully verify your email address, please delete this email.<p>';
-					$from 		= array('support@mystock.la' => 'MyStock.LA');
-					$to 		= $return['User']['email'];
-					$subject 	= 'MyStock.LA - Verify your email address';
+					$Email = new CakeEmail('default');
+					$Email->template('verifyEmail', 'verifyEmail');
+					$Email->viewVars(array('userId' => $userData['User']['id'],'publicKey'=>$publicKey));
+					$Email->emailFormat('both');
+					$Email->from(array('support@mystock.la' => 'MyStock.LA'));
+					$Email->to($userData['User']['email']);
+					$Email->subject('MyStock.LA - Verify your email address');
 
-					if ($this->send_email('default',$from,$to,$subject,$message)) {
-						$return['send-email'] = true;
+					if ($Email->send()) {
+						$return['status'] = 'success';
+					}else{
+						$return['status'] = 'error';
+						$return['message'] = 'email-not-send';
 					}
-
 				}else{
-					$return['saved-user'] = false;
+					$return['status'] = 'error';
+					$return['message'] = 'cannot-save-new-user';
 				}
-
 			}else{
-                $return['already-exist'] = true;
+				$return['status'] = 'error';
+				$return['message'] = 'user-already-exist';
             }
         }else{
-            $return['invalid-data'] = true;
+			$return['status'] = 'error';
+			$return['message'] = 'invalid-data';
         }
 
         $this->{'set'}('return',$return);
         $this->{'render'}('ajax_view','ajax');
     }
 
-    private function email($email){
-        $userEmailConditions = array(
-            'conditions' => array('User.email' => $email),
-        );
-        return $this->{'User'}->find('first',$userEmailConditions);
-    }
 
+	/*
+	 @Name              -> check_email
+	 @Description       -> to seed if email is already in database
+	 @RequestType	    -> AJAX-POST
+	 @Parameters        -> NULL
+	 @Receives       	-> JSON object
+	 @Returns           -> Array, which is presented as json string with the json_encode() function in "ajax_view" view, in the ajax layout.
+	 */
     public function check_email(){
-        if($this->{'request'}->is('post')){
-            $request = $this->{'request'}->data;
-        }else{
-            $request = $this->{'request'}->query;
-        }
+		$request = $this->{'request'}->data;
 
-        $email = $this->email($request['UserEmail']);
+		$userEmailConditions = array(
+				'conditions' => array('User.email' => $request['UserEmail']),
+		);
+
+        $email = $this->{'User'}->find('first',$userEmailConditions);
 
         if($email){
             $return = true;
@@ -228,25 +345,138 @@ class UsersController extends AppController{
         $this->{'render'}('ajax_view','ajax');
     }
 
-	public function  new_password(){
-		debug($this->{'request'});
-
-		//			$newHash = Security::hash($newPassword, 'blowfish', $storedPassword);
-	}
-
-    public function recover_account(){
-        $request = $this->{'request'}->input('json_decode',true);
+	public function set_new_password(){
+		$request = $this->{'request'}->input('json_decode',true);
+		$return = array();
 
 		$options = array(
 				'conditions' => array(
-						'User.email' 	=> $request['email'],
+						'User.id' 		=> $request['id'],
 				)
 		);
 
 		$user = $this->{'User'}->find('first',$options);
 
-		if($user == true){
+		// checks that the user exist
+		if($user){
+			$tempPasswordHash = Security::hash($request['key'], 'blowfish', $user['user']['temp_password']);
 
+			if($tempPasswordHash===$user['user']['temp_password']){
+				Security::setHash('blowfish');
+				$passwordHash = Security::hash($request['password']);
+
+				$publicKey	 	= String::uuid();
+				$privateKeyHash = Security::hash($publicKey);
+
+				$user =	array(
+						'User'=>Array
+						(
+								'password'				=>	$passwordHash,
+								'temp_password'			=>	$privateKeyHash,
+						)
+				);
+
+				if($this->{'User'}->save($user)){
+					$userData = $this->{'User'}->read();
+
+					// Send email to notify what the password has been changed
+
+					$Email = new CakeEmail('default');
+					$Email->template('passwordHasBeenChanged', 'passwordHasBeenChanged');
+//					$Email->viewVars(array('userId' => $userData['User']['id'],'publicKey'=>$publicKey));
+					$Email->emailFormat('both');
+					$Email->from(array('support@mystock.la' => 'MyStock.LA'));
+					$Email->to($userData['User']['email']);
+					$Email->subject('MyStock.LA - You password has been changed');
+//
+					if ($Email->send()) {
+						$return['status'] = 'success';
+					}else{
+						$return['status'] = 'error';
+						$return['message'] = 'email-not-send';
+					}
+
+				}else{
+					$return['status'] = 'error';
+					$return['message'] = 'cannot-set-new-password';
+				}
+
+
+
+			}else{
+				$return['status'] 	= 'error';
+				$return['message']	= 'the-key-is-invalid';
+			}
+		}else{
+			$return['status'] = 'error';
+			$return['message'] = 'user-not-exist';
+		}
+
+
+	}
+
+	/*
+	 @Name              -> new_password_request
+	 @Description       -> To accept or not, set new password
+	 @RequestType	    -> GET;
+	 @Parameters        -> NULL
+	 @Receives       	-> 2 Parameters, id=> is uuid UserId; key=> is uuid temp password which is processed for Security::hash blowfish pipeline.
+	 @Returns           -> Array
+	 */
+	public function  new_password_request(){
+		$request = $this->{'request'}->params;
+
+		$options = array(
+				'conditions' => array(
+						'User.id' 		=> $request['id'],
+				)
+		);
+
+		$user = $this->{'User'}->find('first',$options);
+
+		// checks that the user exist
+		if($user){
+			$tempPasswordHash = Security::hash($request['key'], 'blowfish', $user['user']['temp_password']);
+
+			if($tempPasswordHash===$user['user']['temp_password']){
+				$return['status'] 	= 'success';
+				$return['message']	= 'request-accepted';
+			}else{
+				$return['status'] 	= 'error';
+				$return['message']	= 'this-link-is-invalid';
+			}
+		}else{
+			$return['status'] = 'error';
+			$return['message'] = 'user-not-exist';
+		}
+
+		$this->{'set'}('data',$return);
+	}
+
+	/*
+	 @Name              -> recover_account
+	 @Description       -> for recover account
+	 @RequestType	    -> AJAX-POST;
+	 @Parameters        -> NULL
+	 @Receives       	-> JSON object;
+	 @Returns           -> Array, which is presented as json string with the json_encode() function in "ajax_view" view, in the ajax layout.
+	 */
+    public function recover_account(){
+        $request = $this->{'request'}->input('json_decode',true);
+		$return  = array();
+
+		$options = array(
+			'conditions' => array(
+				'User.email' 	=> $request['email'],
+			)
+		);
+
+		$user = $this->{'User'}->find('first',$options);
+
+		// checks that the user exist
+		if($user){
+
+			// checks that user is not banned
 			if(!$user['User']['banned']){
 				Security::setHash('blowfish');
 
@@ -256,37 +486,39 @@ class UsersController extends AppController{
 				$newTempPassword =	array(
 						'User'=>Array
 						(
-								'id'				=>$user['User']['id'],
-								'email_verified' 	=> 1,
-								'temp_password'		=>	$privateKey,
+							'id'				=>$user['User']['id'],
+							'email_verified' 	=> 1,
+							'temp_password'		=>$privateKey,
 						)
 				);
 
 				if($this->{'User'}->save($newTempPassword)){
 
-					// Send Email
-					$message = '<h3>MyStock.LA - Set new password</h3>'.
-							'<p>Click in the next link to set new password:</p>'.
-							'<p><a href="http://www.mystock.la/np/'.(string)$user['User']['id'].'/'.(string)$publicKey.'" target="_blank">Set new password</a></p>'.
-							'<p>NOTE: After set new password, please delete this email.<p>';
-					$from 		= array('support@mystock.la' => 'MyStock.LA');
-					$to 		= $user['User']['email'];
-					$subject 	= 'Recover password';
+					$Email = new CakeEmail('default');
+					$Email->template('newPasswordRequest', 'newPasswordRequest');
+					$Email->viewVars(array('userId' => $user['User']['id'],'publicKey'=>$publicKey));
+					$Email->emailFormat('both');
+					$Email->from(array('support@mystock.la' => 'MyStock.LA'));
+					$Email->to($user['User']['email']);
+					$Email->subject('MyStock.LA - Set new password');
 
-					if ($this->send_email('default',$from,$to,$subject,$message)) {
-						$return['send-email'] = true;
+					if ($Email->send()) {
+						$return['status'] = 'success';
+					}else{
+						$return['status'] = 'error';
+						$return['message'] = 'email-not-send';
 					}
-
 				}else{
-					$return['send'] = false;
+					$return['status'] = 'error';
+					$return['message'] = 'cannot-set-new-parameters';
 				}
-
 			}else{
-				$return['banned'] 	= true;
+				$return['status'] = 'error';
+				$return['message'] = 'banned';
 			}
-
 		}else{
-			$return['send'] = false;
+			$return['status'] = 'error';
+			$return['message'] = 'user-not-exist';
 		}
 
         $this->{'set'}('return',$return);
---
+++ app/View/Emails/text/new_password_request.ctp
@@ -0,0 +1,1 @@
+http://www.mystock.la/np/<?php echo (string)$userId.'/'.(string)$publicKey; ?>
